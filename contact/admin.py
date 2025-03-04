81b33873abb4f390778c2b5d0d2bd41a9516c728
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-03-04 19:36:52 -0300

    feat: added main contacts page with data and css

--- contact/admin.py
+++ contact/admin.py
@@ -5,14 +5,14 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name','last_name', 'phone', 'category',
-    ordering = 'id',
+    list_display = 'id', 'first_name','last_name', 'phone', 'category', 'show',
+    ordering = '-id',
     # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = False
     list_display_links = 'id', 'phone',
-    # list_editable = 'f irst_name', 'last_name',
+    list_editable = 'first_name', 'last_name', 'show',
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
