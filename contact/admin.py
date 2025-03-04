e3c1f52ddafc356ccdb3c192933cb3e4578e87ae
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-03-04 15:44:19 -0300

    feat: adjust django admin to display category category

--- contact/admin.py
+++ contact/admin.py
@@ -5,7 +5,7 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name','last_name', 'phone',
+    list_display = 'id', 'first_name','last_name', 'phone', 'category',
     ordering = 'id',
     # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
@@ -13,3 +13,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = False
     list_display_links = 'id', 'phone',
     # list_editable = 'f irst_name', 'last_name',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = 'id',
