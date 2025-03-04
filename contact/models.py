d28758cecb7a3513c56fc0babdb067c14ca8f7fd
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-03-04 15:43:52 -0300

    feat: "add field category to contact"

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -10,7 +21,13 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
-    picture = models.ImageField(blank=True, upload_to='picture/%Y/%m/')
+    picture = models.ImageField(blank=True, upload_to="picture/%Y/%m/")
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
 
     def __str__(self) -> str:
-        return (f'{self.first_name} {self.last_name}')
+        return f"{self.first_name} {self.last_name}"
