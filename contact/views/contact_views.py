6f8629d0e10ac25f148ddb069238891792df5ea2
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-03-04 20:45:33 -0300

    feat: added contact page

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+
 
 # Create your views here.
 def index(request):
@@ -14,5 +15,19 @@ def index(request):
     return render(
         request,
         "contact/index.html",
+        context
+    )
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact= get_object_or_404(Contact.objects, pk=contact_id, show=True)
+
+    context = {
+        'contact': single_contact
+    }
+
+    return render(
+        request,
+        "contact/contact.html",
         context
     )
