a75efb53ab1aed495e51d98e5b225ecf4fa6281a
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-03-04 15:28:23 -0300

    feat: add image model + setup project settings

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from contact import models
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name','last_name', 'phone',
+    ordering = 'id',
+    # list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = False
+    list_display_links = 'id', 'phone',
+    # list_editable = 'f irst_name', 'last_name',
