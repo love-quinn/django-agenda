71fe23147d91ed73d2a8490e7538a2d7e9fabac4
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-03-04 16:05:24 -0300

    feat: added owner field on contact model

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -27,6 +28,12 @@ class Contact(models.Model):
         on_delete=models.SET_NULL,
         blank=True,
         null=True
+    )
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
     )
 
     def __str__(self) -> str:
