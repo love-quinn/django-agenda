21ecc0fbbc042281f5daec79edbc3d80efec01f7
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-03-10 15:39:07 -0300

    feat: add flash messages

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from contact.forms import RegisterForm
+from django.contrib import messages
 
 def register(request):
     form = RegisterForm()
+
+    # messages.info(request, 'Um texto que seja')
+    # messages.warning(request, 'Um texto que seja')
+    # messages.error(request, 'Um texto que seja')
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuário registrado')
+            return redirect('contact:index')
+
 
     return render (
         request,
