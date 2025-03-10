68cfd1e6133d568eaa00790bc82dc327cc0a7878
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-03-10 01:31:11 -0300

    feat: added deleted possibility

--- contact/forms.py
+++ contact/forms.py
@@ -4,18 +4,24 @@ from django.core.exceptions import ValidationError
 
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+    widget=forms.TextInput(
+        attrs={
+            'class': 'classe-a classe-b',
+            'placeholder': 'Aqui veio do init',
+        }
+    ),
+    label='Primeiro Nome',
+    help_text='Texto de ajuda para seu usuário',
+    )
+    
     class Meta:
         model = Contact
-        fields = ("first_name", "last_name", "phone")
+        fields = ("first_name", "last_name", "phone", 'email', 'description', 'category')
 
-    def clean(self):
-        cleaned_data = self.cleaned_data
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get("first_name")
 
-        self.add_error(
-            "first_name", ValidationError("Mensagem de erro", code="invalid")
-        )
-
-        self.add_error(None, ValidationError("Mensagem de erro 2", code="invalid"))
-
-        print(cleaned_data)
-        return super().clean()
+        if first_name == "ABC":
+            raise ValidationError("" "Não digite ABC neste campo", code="Inválido")
+        return first_name
