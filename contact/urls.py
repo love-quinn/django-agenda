5089790c90e35d5d5183a1c9fc3dafbb9bf6724f
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-03-10 15:56:10 -0300

    feat: ensure user can login and logout

--- contact/urls.py
+++ contact/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
 
     # User (CRUD)
     path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
 
 
 ]
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,13 +1,11 @@
 from django.shortcuts import redirect, render
 from contact.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def register(request):
     form = RegisterForm()
-
-    # messages.info(request, 'Um texto que seja')
-    # messages.warning(request, 'Um texto que seja')
-    # messages.error(request, 'Um texto que seja')
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -25,3 +23,29 @@ def register(request):
             'form': form,
         }
     )
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request,user)
+            messages.success(request, 'Logado com sucesso')
+            return redirect('contact:index')
+        messages.error(request, 'Login inválido')
+        
+
+    return render (
+        request,
+        'contact/login.html',
+        {
+            'form': form,
+        }
+    )
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
