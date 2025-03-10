8bb8d33c810f82a567927f877103f47f01db67a1
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-03-10 15:26:33 -0300

    feat: create user register form

--- contact/forms.py
+++ contact/forms.py
@@ -1,20 +1,30 @@
 from contact.models import Contact
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 
 class ContactForm(forms.ModelForm):
     picture = forms.ImageField(
         widget=forms.FileInput(
             attrs={
-                'accept': 'image/*',
+                "accept": "image/*",
             }
         )
     )
 
     class Meta:
         model = Contact
-        fields = ("first_name", "last_name", "phone", 'email', 'description', 'category', 'picture')
+        fields = (
+            "first_name",
+            "last_name",
+            "phone",
+            "email",
+            "description",
+            "category",
+            "picture",
+        )
 
     def clean_first_name(self):
         first_name = self.cleaned_data.get("first_name")
@@ -22,3 +32,39 @@ class ContactForm(forms.ModelForm):
         if first_name == "ABC":
             raise ValidationError("" "Não digite ABC neste campo", code="Inválido")
         return first_name
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3
+    )
+
+    last_name = forms.CharField(
+        required=True,
+        min_length=3
+    )
+
+    email = forms.EmailField(
+        required=True,
+        min_length=3
+    )
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "email",
+            "username",
+            "password1",
+            "password2",
+        )
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este email', code='invalid')
+            )
--- contact/urls.py
+++ contact/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     path('contact/create/', views.create, name='create'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+
+    # User (CRUD)
+    path('user/create/', views.register, name='register'),
+
+
 ]
