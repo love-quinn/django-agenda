20aeeaaadc2a94a2d2d8857d12ce4b4e0aa6a6be
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-03-10 16:30:30 -0300

    feat: ensure user can login/logout and control login paged access

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from contact.forms import ContactForm
 from contact.models import Contact
 
-
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -37,7 +38,7 @@ def create(request):
         context
     )
 
-
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
@@ -73,6 +74,7 @@ def update(request, contact_id):
         context
     )
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
